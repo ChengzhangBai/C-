7777c93df6e2ca725596d9b421e94fa54fd8ad15
Chengzhang <74751781+ChengzhangBai@users.noreply.github.com>
2021-09-12 20:55:14 -0400

    Add files via upload

---
+++ AddStudent.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddStudent.aspx.cs" Inherits="Lab7.AddStudent" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" href="App_Themes/SiteStyles.css" />
+    <title>Student</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>
+            Students</h1>
+ 
+           <asp:Table ID="Table1" runat="server">
+               <asp:TableRow runat="server">
+                   <asp:TableCell runat="server" HorizontalAlign="Justify">Student Name:</asp:TableCell>
+                   <asp:TableCell runat="server"><asp:TextBox ID="StudentName" autocomplete="off" runat="server" Width="130px"></asp:TextBox>
+</asp:TableCell>
+                   <asp:TableCell runat="server"><asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="StudentName" ErrorMessage="Required!" Visible="True" ForeColor="Red"></asp:RequiredFieldValidator></asp:TableCell>
+               </asp:TableRow>
+               <asp:TableRow runat="server">
+                   <asp:TableCell runat="server" HorizontalAlign="Justify">Student Type:</asp:TableCell>
+                   <asp:TableCell runat="server"><asp:ListBox ID="StudentType" runat="server" Rows="1" Width="138px">
+                <asp:ListItem Value="">Select......</asp:ListItem>
+                
+<asp:ListItem Value="ft">Full time</asp:ListItem>
+                
+<asp:ListItem Value="pt">Part time</asp:ListItem>
+                
+<asp:ListItem Value="co">Coop</asp:ListItem>
+            
+</asp:ListBox>
+</asp:TableCell><asp:TableCell runat="server"><asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="StudentType" ErrorMessage="Must select one!" Visible="True" ForeColor="Red"></asp:RequiredFieldValidator></asp:TableCell>
+               </asp:TableRow>
+               <asp:TableRow runat="server">
+                   <asp:TableCell runat="server"><asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Add" />
+</asp:TableCell>
+               </asp:TableRow>
+        </asp:Table>
+ 
+                 
+        
+ 
+                 
+   <asp:Label ID="errMsg" runat="server" CssClass="error"></asp:Label>      
+
+<asp:Table ID="tblStudents" runat="server" CssClass="table">
+            <asp:TableRow runat="server">
+                <asp:TableHeaderCell runat="server" HorizontalAlign="Center" Width="20px">ID</asp:TableHeaderCell>
+                <asp:TableHeaderCell runat="server" HorizontalAlign="Center">Student Name</asp:TableHeaderCell>
+                <asp:TableHeaderCell runat="server" HorizontalAlign="Center">Type</asp:TableHeaderCell>
+
+        
+            </asp:TableRow></asp:Table>
+        <br />
+        <a href="RegisterCourse.aspx">Register Courses</a></form>
+</body>
+</html>
---
+++ AddStudent.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Lab8.Models;
+
+namespace Lab7
+{
+    public partial class AddStudent : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+         if (!IsPostBack)
+            {
+                showCell();
+            }
+        }
+        public void nullRec()
+        {
+            genCell(0, "<div class=\"error\">No student yet</div>","");
+        }
+        public bool chkName(string x) =>x=="";  
+        static List<Student> stuLst = new List<Student>();
+        
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            StudentName.BackColor = Color.White;
+            if (chkName(StudentName.Text.Trim())) {
+                errMsg.Text = "Student name cannot be empty!";
+                StudentName.BackColor = Color.Yellow;
+                StudentName.Focus();
+            }
+            else {
+                errMsg.Text = "";
+            int sID = GenRandomID(); 
+            string sName = StudentName.Text.Trim();
+            string sType = StudentType.SelectedValue;
+            int? maxWeeklyHours = null;
+            int? maxNumOfCourses = null;
+                switch (sType)
+                {
+                    case "ft":
+                        FullTimeStudent fStu = new FullTimeStudent(sID, sName, maxWeeklyHours);
+                        stuLst.Add(fStu);
+                        
+                        break;
+                    case "pt":
+                        PartTimeStudent pStu = new PartTimeStudent(sID, sName, maxNumOfCourses);
+                        stuLst.Add(pStu);
+                        break;
+                    case "co":
+                        CoopStudent cStu = new CoopStudent(sID, sName, maxWeeklyHours, maxNumOfCourses);
+                        stuLst.Add(cStu);
+                      
+                        break;
+                    default:
+                        errMsg.Text = "Please select a type!";
+                        showCell();
+                        StudentType.BackColor = Color.Yellow;
+                        return;
+                }
+
+            
+            Session.Add("studentList", stuLst);
+            }
+            if (Session["studentList"] == null)
+            {
+                nullRec();
+                return;
+            }
+
+            showCell();
+            StudentName.Text = "";
+            StudentType.SelectedValue = "";
+            StudentName.BackColor = StudentType.BackColor = Color.White;
+            StudentName.Focus();
+         }
+        
+        public void nameChanged(object sender, EventArgs e)
+        {
+            if (chkName(StudentName.Text.Trim()))
+            {
+                errMsg.Text = "Student name cannot be empty!";
+                StudentName.BackColor = Color.Yellow;
+                StudentName.Focus();
+            }
+            else
+            {
+                errMsg.Text = "";
+            }
+            showCell();
+
+        }
+        public void showCell()
+        {
+            if (Session["studentList"] == null)
+            {
+                genCell(0, "<div class=\"error\">No student yet</div>","");
+                return;
+            }
+    
+            foreach (Student stud in (List<Student>)Session["studentList"])
+            {
+
+                string type="";
+                if (stud.GetType().ToString().Contains("FullTimeStudent"))
+                {
+                    type = "Full Time";
+                }
+                if (stud.GetType().ToString().Contains("PartTimeStudent"))
+                {
+                    type = "Part Time";
+                }
+                if (stud.GetType().ToString().Contains("CoopStudent"))
+                {
+                    type = "Coop";
+                }
+
+                genCell(stud.Id,stud.Name,type);
+             }
+         }
+        public void genCell(int id, string name,string type)
+        {
+            TableRow rowItem = new TableRow();
+            tblStudents.Rows.Add(rowItem);
+            rowItem.Style.Add("text-align", "center");
+            TableCell cellItem = new TableCell();
+            rowItem.Cells.Add(cellItem);
+            cellItem.Text = id == 0 ? "" : id.ToString();
+            TableCell cellItemtwo = new TableCell();
+            rowItem.Cells.Add(cellItemtwo);
+            cellItemtwo.Text = name;
+            TableCell cellItemThree = new TableCell();
+            rowItem.Cells.Add(cellItemThree);
+            cellItemThree.Text = type;
+        }
+
+        public int GenRandomID()
+        {
+            Random rd = new Random();
+            int randomNum = rd.Next(100000,1000000);
+            return randomNum;
+        }
+    }
+}
